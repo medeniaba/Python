7c68757b24b5145f1db7b04a50b08b8c386166da
Medeni Aba <135317895+medeniaba@users.noreply.github.com>
2023-06-21 11:59:23 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
 
 
 #Enumerated'i for kısmına yazdıgımızda output çıktının index sırasını yazdırmaya yarar .
@@ -11,21 +8,20 @@ for e in adlar :
     print(e)
 
 
-# In[7]:
+
 
 
 for i,e in enumerate(adlar):
     print(i,"indisinde --> ",  e)
 
 
-# In[9]:
+
 
 
 for i , e in enumerate(adlar , start = 1):
     print(i , " . lokasyonunda bulunan eleman:",e )
 
 
-# In[14]:
 
 
 # zip() 2 farklı listeyi birşeltirmeye yarayan bir fonksiyondur 
@@ -41,7 +37,6 @@ for k,v in zip(keys,values):
 d    
 
 
-# In[17]:
 
 
 #Exercies 3 
@@ -50,7 +45,6 @@ a = [] and 2
 print(not a)
 
 
-# In[ ]:
 
 
 
